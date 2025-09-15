d81e72ed0da138ca760457399e123d7649070fbc
Vicente118 <vdarras27@gmail.com>
2025-09-15 17:49:17 +0200

    Ready

--- test_fake.sh
+++
@@ -1,4 +0,0 @@
-while true
-do 
-    ./run.sh /usr/bin/time -v ./malloc 2> out_fake ; cat out_fake | grep Minor
-done
--- test_true.sh
+++
@@ -1,4 +0,0 @@
-while true
-do 
-    /usr/bin/time -v ./malloc_sys 2> out_true ; cat out_true | grep Minor
-done
